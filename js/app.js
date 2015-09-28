ecb7d71b4ea5cab26c3f05e9b27841e628e9e1c4
Hari haran <hariharan.uno@gmail.com>
2015-09-28 17:07:03 +0530

    Use react-router
    Although its a contrived example, its a start

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, Link } from 'react-router'
+import '../css/muzzle.css' // Try loading common css in html itself
 
+const App = (props) => {
+  return (
+      <div>
+      <br/>
+      <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/inbox">Inbox</Link></li>
+      </ul>
+      {props.children}
+    </div>
+  )
+}
 
-var Aquarium = (props) => {
-  var getFish = (fish) => {
-    return fish
-  };
-  var fish = getFish(props.species);
-  return <p>{fish}</p>;
-};
+const About = (props) => {
+  return <p>About</p>
+}
+
+const Inbox = (props) => {
+  return <p>Inbox</p>
+}
+
+const routes = {
+  path: '/',
+  component: App,
+  childRoutes: [
+    { path: 'about', component: About },
+    { path: 'inbox', component: Inbox },
+  ],
+}
 
 ReactDOM.render(
-    <Aquarium species="Gold Fish" />,
+    <Router routes={routes} />,
   document.getElementById('example')
-);
+)
