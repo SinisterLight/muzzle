97721d18fc95f8f98962c29fad4a4b40f62ebe62
Hari haran <hariharan.uno@gmail.com>
2015-09-28 12:38:42 +0530

    Use eslint instead of jshint
    Also, add rules specific to react, mostly
    copied from facebook/react/.eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-    entry: './entry.js',
-    output: {
-	filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-	loaders: [
-	    { test: /\.css$/, loaders: ['style', 'css']},
-	    { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
-	],
-	postLoaders: [
-	    {test: /\.jsx?$/, exclude: /node_modules/, loader: 'jshint-loader'}
-	]
-    }
+  entry: './entry.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    preLoaders: [
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"}
+    ],
+    loaders: [
+      {test: /\.css$/, loaders: ['style', 'css']},
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  eslint: {
+    configFile: './.eslintrc'
+  }
 }
