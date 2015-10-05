dd834599510a102316e2d0b61af8a326c421afd0
Hari haran <hariharan.uno@gmail.com>
2015-10-05 16:50:40 +0530

    Indent test data and add a TODO stub

--- js/app.jsx
+++ js/app.jsx
@@ -73,9 +73,12 @@ const About = (props) => {
 }
 
 const Agents = (props) => {
-  var testData = [{'uid':'528ba2ef43ae','host_name':'vision','registered_at':'2015-09-04T17:07:00.914+05:30','updated_at':'2015-09-16T13:00:03.712+05:30','status':'online'},
+  // TODO: Get the data from marksman's API
+  var testData = [
+    {'uid':'528ba2ef43ae','host_name':'vision','registered_at':'2015-09-04T17:07:00.914+05:30','updated_at':'2015-09-16T13:00:03.712+05:30','status':'online'},
     {'uid':'526ba2ef43ae','host_name':'ironlegion_message_queue','registered_at':'2015-09-04T17:07:00.914+05:30','updated_at':'2015-09-15T17:05:47.789+05:30','status':'online'},
-    {'uid':'524ba2ef43ae','host_name':'jarvis_loadbalancer_nginx','registered_at':'2015-09-04T17:07:00.914+05:30','updated_at':'2015-09-15T17:05:47.789+05:30','status':'offline'}];
+    {'uid':'524ba2ef43ae','host_name':'jarvis_loadbalancer_nginx','registered_at':'2015-09-04T17:07:00.914+05:30','updated_at':'2015-09-15T17:05:47.789+05:30','status':'offline'}
+  ];
   return <AgentsStatus data={testData}/>
 }
 
